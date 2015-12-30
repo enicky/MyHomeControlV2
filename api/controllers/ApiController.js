32ce7f79a3b8ae43ccd5471f056cecb410d895e0
enicky <enicky80@gmail.com>
2015-12-30 08:43:11 +0100

    added filter on V_LIGHT on dashboard

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -25,7 +25,7 @@ module.exports = {
     },
 
     sensorsIndex : function(req, res){
-        Sensor.find({deviceTypeString : {'!' : 'V_LIGHT'}}).sort({internalid : 'desc'}).exec(function(err, sensors){
+        Sensor.find({deviceTypeString : {'!' : 'S_LIGHT'}}).sort({internalid : 'desc'}).exec(function(err, sensors){
             if(err) sails.log('error', 'err : ', err);
             sails.log('debug','Sensors: ', sensors);
             return res.send(sensors);
