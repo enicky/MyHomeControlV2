44b9d0769269c67fbd562cec97e3cdd5c2ed62b9
enicky <enicky80@gmail.com>
2016-05-28 12:07:02 +0200

    Fix on update sensor

--- api/controllers/AngularController.js
+++ api/controllers/AngularController.js
@@ -15,22 +15,17 @@ module.exports = {
         var sensorsToReturn = [];
         Sensor.find({sort : 'internalid'}).exec(function(err, sensors){
             async.each(sensors, function(sensor, cb){
-                sails.log.info('[AngularController:getDashboardSensors] get reading for : ', sensor.internalid);
-                sails.log.info('[AngularController:getDashboardSensors] get reading for : ', sensor.sensorId, sensor.deviceId);
                 Reading.find({
                     where: {internalId: sensor.internalid},
                     sort: 'createdAt DESC',
                     limit : 1
                 }).exec(function(err, reading){
-                    sails.log.info("[AngularController:getDashboardSensors] reading : ", reading);
                     sensor.lastReading = reading && reading.length > 0 ? reading[0].value : -1;
                     sensor.lastReadingDate = reading && reading.length > 0 ? reading[0].createdAt : 0;
                     sensorsToReturn.push(sensor);
-                    sails.log.info("[AngularController:getDashboardSensors] sensorsToReturn length : ", sensorsToReturn.length);
                     return cb();
                 })
             }, function(err){
-                sails.log.info("[AngularController:getDasboardSensors] sensorsToReturn : ", sensorsToReturn);
                 return res.json(sensorsToReturn);
             })
         })
