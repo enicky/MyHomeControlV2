4ec04912575c4f125a8a303e13377dbd9c3da71c
enicky <enicky80@gmail.com>
2016-07-16 08:18:31 +0200

    Added Sentry Functionality

---
+++ api/hooks/Sentry/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by nicholase on 16/07/16.
+ */
+module.exports = function mySentryHook(sails) {
+    return {
+
+        // Runs automatically when the hook initializes
+        initialize: function (cb) {
+            sails.log('debug','[Sentry:initialize] Start init hook');
+            var raven = require('raven');
+            var client = new raven.Client(process.env.SENTRY_URL);
+            client.patchGlobal();
+
+            sails.log('debug','finished start hook');
+            return cb();
+        }
+    }
+}
